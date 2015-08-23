9ac8345448ef1bee2566664a9af1bc9cb47beb2d
GuibuA <guibuadrian@gmail.com>
2015-08-23 20:52:52 -0300

    Adapto las clases como estan en el tp

---
+++ src/com/utn/sistemas/qch/entities/Condimento.java
@@ -0,0 +1,15 @@
+package com.utn.sistemas.qch.entities;
+
+public class Condimento
+{
+
+	public String nombre;
+	public String tipo;
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/utn/sistemas/qch/entities/Ingrediente.java
+++ src/com/utn/sistemas/qch/entities/Ingrediente.java
@@ -2,30 +2,33 @@ package com.utn.sistemas.qch.entities;
 
 import com.utn.sistemas.qch.dao.IngredienteDAO;
 
-public class Ingrediente {
+public class Ingrediente
+{
 
-    private static IngredienteDAO ingredienteDAO;
+	private static IngredienteDAO ingredienteDAO;
 
-    private String nombre;
-    private Integer nivelPiramideAlimenticia;
+	public String nombre;
+	public Integer nivelPiramideAlimenticia;
+	public Integer contraindicaciones;
+	public Integer caloriasXPorcion;
 
-    public static Ingrediente buscarIngrediente(Integer ingredienteId) {
-        return ingredienteDAO.obtenerIngredientePorNombre(ingredienteId);
-    }
+	public static Ingrediente buscarIngrediente(Integer ingredienteId)
+	{
+		return ingredienteDAO.obtenerIngredientePorNombre(ingredienteId);
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public void setNombre(String nombre)
+	{
+		this.nombre=nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public Integer obtenerNivelPiramideAlimenticia()
+	{
+		return nivelPiramideAlimenticia;
+	}
 
-    public Integer obtenerNivelPiramideAlimenticia() {
-        return nivelPiramideAlimenticia;
-    }
-
-    public void setNivelPiramideAlimenticia(Integer nivelPiramideAlimenticia) {
-        this.nivelPiramideAlimenticia = nivelPiramideAlimenticia;
-    }
+	public void setNivelPiramideAlimenticia(Integer nivelPiramideAlimenticia)
+	{
+		this.nivelPiramideAlimenticia=nivelPiramideAlimenticia;
+	}
 }
